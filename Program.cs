70464e036d0e687215e16c81a4bd54fa3890a83c
OBones <obones@free.fr>
2019-10-23 23:12:16 +0200

    MPL header

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ Linky Cmd - The command line program to be used with LinkyPIC
+
+ The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
+ you may not use this file except in compliance with the License. You may obtain a copy of the
+ License at http://www.mozilla.org/MPL/
+
+ Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+ ANY KIND, either express or implied. See the License for the specific language governing rights
+ and limitations under the License.
+
+ The Original Code is Program.cs.
+
+ The Initial Developer of the Original Code is Olivier Sannier.
+ Portions created by Olivier Sannier are Copyright (C) of Olivier Sannier. All rights reserved.
+*/
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
